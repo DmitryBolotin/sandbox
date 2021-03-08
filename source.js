597be9141a360702bb5ef1ff622759a325648cfa
Dmitry Bolotin <dbolotin@mail.ru>
2021-03-08 16:54:05 +0300

    one file

--- source.js
+++ source.js
@@ -1,21 +1,21 @@
-function loginMessage(success, failure) {
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-    var success = document.getElementsByClassName('successMessage')[0];
-    var failure = document.getElementsByClassName('failureMessage')[0];
-    if (username === 'test' && password === '123') {
-        success.setAttribute("style", "color:green");
-        success.removeAttribute("hidden");
-    }
-    else {
-        failure.setAttribute("style", "color:red");
-        failure.removeAttribute("hidden");
-    }
-}
+// function loginMessage(success, failure) {
+//     var username = document.getElementById('username').value;
+//     var password = document.getElementById('password').value;
+//     var success = document.getElementsByClassName('successMessage')[0];
+//     var failure = document.getElementsByClassName('failureMessage')[0];
+//     if (username === 'test' && password === '123') {
+//         success.setAttribute("style", "color:green");
+//         success.removeAttribute("hidden");
+//     }
+//     else {
+//         failure.setAttribute("style", "color:red");
+//         failure.removeAttribute("hidden");
+//     }
+// }
 
-function clearMessage() {
-    var success = document.getElementsByClassName('successMessage')[0];
-    var failure = document.getElementsByClassName('failureMessage')[0];
-    success.setAttribute("hidden","true");
-    failure.setAttribute("hidden","true");
-}
+// function clearMessage() {
+//     var success = document.getElementsByClassName('successMessage')[0];
+//     var failure = document.getElementsByClassName('failureMessage')[0];
+//     success.setAttribute("hidden","true");
+//     failure.setAttribute("hidden","true");
+// }
