95fa4650713d620847329bae07644d76f01b15e8
Dmitry Bolotin <dbolotin@mail.ru>
2021-03-08 16:43:10 +0300

    update js

--- source.js
+++ source.js
@@ -1,3 +1,21 @@
-function loginMessage() {
-    alert("aloha");
+function loginMessage(success, failure) {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    var success = document.getElementsByClassName('successMessage')[0];
+    var failure = document.getElementsByClassName('failureMessage')[0];
+    if (username === 'test' && password === '123') {
+        success.setAttribute("style", "color:green");
+        success.removeAttribute("hidden");
+    }
+    else {
+        failure.setAttribute("style", "color:red");
+        failure.removeAttribute("hidden");
+    }
+}
+
+function clearMessage() {
+    var success = document.getElementsByClassName('successMessage')[0];
+    var failure = document.getElementsByClassName('failureMessage')[0];
+    success.setAttribute("hidden","true");
+    failure.setAttribute("hidden","true");
 }
